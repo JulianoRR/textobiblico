55c18011114f6fd5fad45f5cb1c35fcade5f6c9c
JulianoRR <30480906+JulianoRR@users.noreply.github.com>
2024-10-28 14:30:49 -0300

    update fetch -> mode: "cors"

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const content = document.querySelector('#content')
 
 async function newConsulta(input) {
 	const responsenew = await fetch("https://api-textobiblico.vercel.app/api/search/new", {
+		mode: "cors",
 		method: "POST",
 		body: JSON.stringify({"input": input}),
 		headers: {
@@ -17,7 +18,7 @@ async function newConsulta(input) {
 	//console.log(datanew.success?datanew.value:datanew.message)
 
 	if(!datanew.success){return}
-	const responseget = await fetch(`https://api-textobiblico.vercel.app/api/search/${datanew.value}`, { method: "GET" })
+	const responseget = await fetch(`https://api-textobiblico.vercel.app/api/search/${datanew.value}`, { mode: "cors", method: "GET" })
 	const dataget = await responseget.json()
 	console.log(dataget.success?dataget.value:dataget.message)
 	dataget.value.response.map(res => {
